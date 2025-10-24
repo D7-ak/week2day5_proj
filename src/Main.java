7dc118267667c3767530f53335297b50de3b0f7d
Abdulrahman <alshahrani996655@gmail.com>
2025-10-25 00:53:32 +0300

    V1.0.1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -19,35 +20,122 @@ public class Main {
         xoarr.get(2).add("9");
         boolean isdone = false;
 
-        while (!isdone){
+        while (!isdone) {
+            Random rand = new Random();
 
-            for(int i = 0;i<3;i++){
-                for(int j = 0;j<3;j++){
-                    System.out.print(" | "+xoarr.get(i).get(j)+" | ");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(" | " + xoarr.get(i).get(j) + " | ");
                 }
                 System.out.print("\n______________________\n");
-
             }
-            System.out.println("where :");
-            String player = sc.next();
-            if(player.matches("[1-9]")){
-                for(int i = 0;i<3;i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (player.equalsIgnoreCase(xoarr.get(i).get(j))){
-                            xoarr.get(i).set(j,"x");
+            System.out.println("where to play:");
+            String player = sc.next();//taking the input from the player
+            boolean islegal = isleagalPlayer(xoarr,player);
+            if(isdone(xoarr)){
+                break;
+            }
+            if(winner(xoarr)){
+                break;
+            }
+            if(islegal){//we check here the random, is it leagal to be played or not
+                boolean pclegal = false;
+                while (!pclegal){
+                    int pcRand = rand.nextInt(9)+1;
+                    String pcRand_TOstring = String.valueOf(pcRand);
+                    for(int i = 0;i<3;i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if(xoarr.get(i).get(j).matches(pcRand_TOstring)){
+                                System.out.println("pc generated :"+pcRand_TOstring);
+                                xoarr.get(i).set(j, "o");
+                                pclegal =true;
+                                break;
                         }
                     }
                 }
-            }else
-                System.out.println("please enter a number from 1-9");
 
-        isdone = isdone(xoarr);
+                }
+                if (winner(xoarr)){
+                    break;
+                }
+
+                System.out.println("legal");
+            }
 
         }
-
-
     }
+    static boolean winner(ArrayList<ArrayList<String>> xo){
+            if((xo.get(0).get(0).matches("x") && xo.get(0).get(1).matches("x") && xo.get(0).get(2).matches("x"))||
+               (xo.get(1).get(0).matches("x") && xo.get(1).get(1).matches("x") && xo.get(1).get(2).matches("x"))||
+               (xo.get(2).get(0).matches("x") && xo.get(2).get(1).matches("x") && xo.get(2).get(2).matches("x")))
+            {
+                System.out.println("x wins");
+                return true;
+            }
+       else if((xo.get(0).get(0).matches("x") && xo.get(1).get(0).matches("x") && xo.get(2).get(0).matches("x"))||
+               (xo.get(0).get(1).matches("x") && xo.get(1).get(1).matches("x") && xo.get(2).get(1).matches("x"))||
+               (xo.get(0).get(2).matches("x") && xo.get(1).get(2).matches("x") && xo.get(2).get(2).matches("x")))
+            {
+                System.out.println("x wins");
+                return true ;
+            }
+        else if((xo.get(0).get(0).matches("x") && xo.get(1).get(1).matches("x") && xo.get(2).get(2).matches("x"))||
+                (xo.get(2).get(0).matches("x") && xo.get(1).get(1).matches("x") && xo.get(0).get(2).matches("x")))
+        {
+            System.out.println("x wins");
+            return true;
+        }
+       else if((xo.get(0).get(0).matches("o") && xo.get(0).get(1).matches("o") && xo.get(0).get(2).matches("o"))||
+               (xo.get(1).get(0).matches("o") && xo.get(1).get(1).matches("o") && xo.get(1).get(2).matches("o"))||
+               (xo.get(2).get(0).matches("o") && xo.get(2).get(1).matches("o") && xo.get(2).get(2).matches("o")))
+        {
+            System.out.println("o wins");
+            return true;
+        }
+        else if((xo.get(0).get(0).matches("o") && xo.get(1).get(0).matches("o") && xo.get(2).get(0).matches("o"))||
+                (xo.get(0).get(1).matches("o") && xo.get(1).get(1).matches("o") && xo.get(2).get(1).matches("o"))||
+                (xo.get(0).get(2).matches("o") && xo.get(1).get(2).matches("o") && xo.get(2).get(2).matches("o")))
+        {
+            System.out.println("o wins");
+            return true ;
+        }
+        else if((xo.get(0).get(0).matches("o") && xo.get(1).get(1).matches("o") && xo.get(2).get(2).matches("o"))||
+                (xo.get(2).get(0).matches("o") && xo.get(1).get(1).matches("o") && xo.get(0).get(2).matches("o")))
+        {
+            System.out.println("o wins");
+            return true;
+        }
+        return false;
+    }
+
+
+
+    static boolean isleagalPlayer(ArrayList<ArrayList<String>> xo,String playerChois){
+        if (playerChois.matches("[1-9]")) {
+            boolean played = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (playerChois.equalsIgnoreCase(xo.get(i).get(j))) {
+                            xo.get(i).set(j, "x");
+                            played = false;
+                            return true;
+
+                    }
+                }
+            }
+            if(played){
+                System.out.println("this place is played  ");
+            }
+        } else{
+            System.out.println("please enter a number from 1-9");
+            return false;
+        }
+
+        return false;
+    }
+
     static boolean isdone(ArrayList<ArrayList<String>> xo){
+
 
         for(int i =0;i<3;i++){
             for(int j =0;j<3;j++){
@@ -56,6 +144,7 @@ public class Main {
                 }
             }
         }
+        System.out.println("its a tie");
         return true;
     }
 }
